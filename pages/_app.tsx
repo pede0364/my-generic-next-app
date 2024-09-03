4e635505fee6f02cebfa154a3d98c7fc51139217
Aimee Pederson <aimee.pederson@gmail.com>
2024-09-03 15:57:38 -0500

    stock next app

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+{
+  /* define the global layout */
+}
+
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
